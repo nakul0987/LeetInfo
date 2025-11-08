0b153bcbe290df9c62dc7f1d4e7b742e34dc1ec0
nakul sharma <nakulmac12gmail.com@Nakuls-MacBook-Air.local>
2025-11-08 23:13:59 +0530

    Updated Version

--- app.js
+++ app.js
@@ -2,24 +2,47 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     const inputArea=document.querySelector('#inputBox');
     const searchButton=document.querySelector('.searchBtn');
+
+    const rankRate=document.querySelector('.rankRate');
+
+    const totalSolved=document.querySelector('.totalSolved');
+    const totalQues=document.querySelector('.totalQues');
+
     const eSolved=document.querySelector('.easySolved');
+    const totalE=document.querySelector('.totalEasy');
+
     const mSolved=document.querySelector('.mediumSolved');
+    const totalM=document.querySelector('.totalMedium');
+
     const hSolved=document.querySelector('.hardSolved');
+    const totalH=document.querySelector('.totalHard');
 
 
     searchButton.addEventListener('click',()=>{
         let username=inputArea.value;
-        const leetCodeUrl=`https://leetcode-stats-api.herokuapp.com/${username}`;
+        const leetCodeUrl=`https://leetcode-stats-api.vercel.app/${username}`;
         // console.log(username);
 
         async function getInfo() {
+
             const res=await fetch(leetCodeUrl);
             const data=await res.json();
             console.log(data);
 
+            rankRate.innerHTML=data.ranking;
+
+            totalSolved.innerHTML=data.totalSolved;
+            totalQues.innerHTML=data.totalQuestions;
+
             eSolved.innerHTML=data.easySolved;
+            totalE.innerHTML=data.totalEasy;
+
             mSolved.innerHTML=data.mediumSolved;
+            totalM.innerHTML=data.totalMedium;
+
+
             hSolved.innerHTML=data.hardSolved;
+            totalH.innerHTML=data.totalHard;
 
         }
 
@@ -30,9 +53,6 @@ document.addEventListener('DOMContentLoaded',()=>{
             getInfo();
         }
 
-
-
     });
-
 
 });
